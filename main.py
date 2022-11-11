3546be46765c50e3ddb8e98d7c3350f5ab31ad61
Sabarno Chatterjee <sabarno.05@gmail.com>
2022-11-11 11:02:05 +0530

    Fixed double letters main.py

--- main.py
+++ main.py
@@ -18,29 +18,29 @@ for i in range(0, len(pick)):
   display.append("_")
 print(display)
 
-while not_win:
-  player_guess = input("\nPlease guess a letter.\n\n").lower()
 
+while not_win:
+  print(art.stages[lives])
+  player_guess = input("\nPlease guess a letter.\n\n").lower()
+  
+  if player_guess in display:
+    print("\n\nYou already guess that.")
+  
   count = 0
   for letter in pick:
     count += 1
     if player_guess == letter:
-      if player_guess in display:
-        print("\n\nYou already guess that.")
-      else:
-        display[count-1] = letter
-    
-    
-    
+      display[count-1] = letter
+      
   print(f"\n{display}\n") 
-
+  
   if "_" not in display:
         not_win = False
         print("You won!")
   if player_guess not in pick:
       print("You are wrong\n") 
       lives -= 1
-      #print(art.stages[lives])
+      print(art.stages[lives])
       if lives == 0:
         print("\n You lost! \n\n")
         not_win = False
@@ -48,9 +48,11 @@ while not_win:
         print(f"You lost a life. Lives left: {lives}")
   
   
-  
+
       
-# Need sort out words with double letters.
+      
 # I need to add section wise hints.
+# Need to display the hangman's noose at the beginning.
+# Need to work on double printing nooses.
 
 
